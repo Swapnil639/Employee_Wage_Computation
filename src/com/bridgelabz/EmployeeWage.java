9f8d39cca4dd57f8ed3cf84a1f012ea3309e5dab
Swapnil639 <swapnilpatil631997@gmail.com>
2022-08-17 16:59:57 +0530

    Added :using Switch case EmpWage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,21 +1,28 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-    public static final int PART_TIME_WORK=1;
-    public static final int FULL_TIME_WORK=2;
-    public static final int RATE_PER_HOUR=20;
+    public static final int PART_TIME_WORK = 1;
+    public static final int FULL_TIME_WORK = 2;
+    public static final int RATE_PER_HOUR = 20;
 
     public static void main(String[] args) {
-        int employeeHrs=0;
-        int employeeWage=0;
-        double empCheck=Math.floor(Math.random()*10)%3;
-        if(empCheck==FULL_TIME_WORK)
-            employeeHrs=8;
-        else if(empCheck==PART_TIME_WORK)
-            employeeHrs=4;
-        else
-            employeeHrs=0;
-        employeeWage=employeeHrs*RATE_PER_HOUR;
-        System.out.println("Employee Wage :"+employeeWage);
+        int employeeHrs = 0;
+        int employeeWage = 0;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case FULL_TIME_WORK:
+                employeeHrs = 8;
+                break;
+            case PART_TIME_WORK:
+                employeeHrs = 4;
+                break;
+            default:
+                employeeHrs = 0;
+                break;
+        }
+        employeeWage = employeeHrs * RATE_PER_HOUR;
+        System.out.println("Employee Wage :" + employeeWage);
+
     }
+
 }
