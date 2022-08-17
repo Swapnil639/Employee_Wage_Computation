ac97eb452848f74503755ac65ca6f798838c11c6
Swapnil639 <swapnilpatil631997@gmail.com>
2022-08-17 16:50:38 +0530

    Added :Check employee present or not

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,7 +1,13 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+    public static final int FULL_TIME_WORK = 1;
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation");
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == FULL_TIME_WORK)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
     }
 }
